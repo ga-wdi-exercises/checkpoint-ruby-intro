35ae626924ab7812a4eea5d4bde0fe8a0fdd40df
John Master <john.s.master@gmail.com>
2017-04-09 16:37:19 -0400

    add checkpoint questions

---
+++ code/collections.rb
@@ -0,0 +1,15 @@
+# collections
+
+# 1. In a variable `point1` create a hash representing a coordinate with an `x` and `y` integer values
+
+# 2. Print the y value
+
+# 3. Make an array that includes `point1` and two more points and assign to a variable `points` (`points.length` should be 3)
+
+# 4. Use `.each` to print the y value of each point
+
+# 5. Use `.each_with_index` and string interpolation to print for each point "The point at index <index> has coordinates {x: <x-val>, y: <y-val>}"
+
+# 6. Create an array and call it `distances_from_origin` by mapping over the points and returning the distance between the point and origin, (0,0).
+# Distance from origin can be found by the equation `distance_from_origin = Math.sqrt(x_val ** 2 + y_val ** 2)`
+
---
+++ code/fundamentals.rb
@@ -0,0 +1,15 @@
+# Add code immediately after question. Example:
+
+# Ex. Print instructions to the terminal
+
+message = "Be sure to run your code regularly to see that it works. Keep trying if you get an error. Read the error carefully and remember that the line number of the error can be later than the mistake in the code. If you are stumped by an error, as a comment, add the error and your interpretation of it after the question. Be sure to also comment out your code so that when you run the code on subsequent sessions, you don't get noise from those errors."
+
+puts message
+
+# 1. Create a variable `my_favorite_animal` and assign it the name of your favorite animal
+
+# 2. Print "My favorite animal is ..." and interpolate your favorite animal
+
+# 3. Write a method `make_noise` which prints the string "BANG!"
+
+# 4. Write a method `count_down` which takes one argument and prints the given number and every number from it to 0. Assume all input to be positive integers
