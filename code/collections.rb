c0f0df558be44575a23952267fa9c4fc3d901940
Andres Arango <andresarangov17@gmail.com>
2017-04-10 09:53:53 -0400

    finish checkpoint

--- code/collections.rb
+++ code/collections.rb
@@ -1,15 +1,17 @@
 # collections
 
 # 1. Create a variable `point1` and assign it a hash representing a coordinate with keys `:x` and `:y` which have integer values
-
+point1 = {x: 2, y:2}
 # 2. Print the y value
-
+puts point1[:y]
 # 3. Make an array, `points`, that includes `point1` and two more points (`points.length` should be 3)
-
+points = [point1, {x: 3, y: 3}, {x: 4, y: 4}]
 # 4. Use `.each` to print the y value of each point
-
+points.each {|point| puts point}
+# points.each{|point| puts point[:y]}
+points.each {|point| puts point[:y]}
 # 5. Use `.each_with_index` and string interpolation to print for each point "The point at index <index> has coordinates {x: <x-val>, y: <y-val>}"
-
+points.each_with_index {|point, index| puts "The point at index #{index} has coordinates {x: #{point[:x]}, y: #{point[:y]}}"}
 # 6. Create an array and call it `distances_from_origin` by mapping over the points with the `.map` method and returning the distance between the point and origin, (0,0).
 # Distance from origin can be found with the equation `distance_from_origin = Math.sqrt(x_val ** 2 + y_val ** 2)`
-
+distances_from_origin = points.map {|point| Math.sqrt(point[:x] ** 2 + point[:y] ** 2)}
