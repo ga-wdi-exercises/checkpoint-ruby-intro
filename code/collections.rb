eb930a1663c3e9a6b182fcf2a945267b0cb4378e
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-04-10 10:01:30 -0400

    refined

--- code/collections.rb
+++ code/collections.rb
@@ -12,7 +12,7 @@ points= [point1, point2, point3]  assuming point2= {x:27, y:30} point3= {x:31, y
 # 4. Use `.each` to print the y value of each point
 points.each {|point| puts point [:y]}
 # 5. Use `.each_with_index` and string interpolation to print for each point "The point at index <index> has coordinates {x: <x-val>, y: <y-val>}"
-points.each_with_index {|point, index,| puts "The point at index #{index} has coordinates #{points}"}
+points.each_with_index {|point, index| puts "The point at index #{index} has coordinates #{point}"}
 
 # 6. Create an array and call it `distances_from_origin` by mapping over the points with the `.map` method and returning the distance between the point and origin, (0,0).
 # Distance from origin can be found with the equation `distance_from_origin = Math.sqrt(x_val ** 2 + y_val ** 2)`
