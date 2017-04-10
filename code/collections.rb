9679c7caff4b577a1194daef09b2ce62b3113815
Abrahim <aeaghannam@aol.com>
2017-04-10 12:23:15 -0400

    Abe code add

--- code/collections.rb
+++ code/collections.rb
@@ -2,14 +2,37 @@
 
 # 1. Create a variable `point1` and assign it a hash representing a coordinate with keys `:x` and `:y` which have integer values
 
+point1 = {
+  :x => "38.9072 N",
+  :y => "77.0369 W",
+}
+
+
 # 2. Print the y value
+
+print.point(y)
 
 # 3. Make an array, `points`, that includes `point1` and two more points (`points.length` should be 3)
 
+points.length = {
+  ':x' = '28.9072 N'
+  ':y' =  '47.0369 W'
+}
+
+points = 'point1' + 'ponts.length'
+
 # 4. Use `.each` to print the y value of each point
 
+print points(y).each
+
 # 5. Use `.each_with_index` and string interpolation to print for each point "The point at index <index> has coordinates {x: <x-val>, y: <y-val>}"
+
+print "The point at index <index> has coordinates {x: <x-val>, y: <y-val>}"
 
 # 6. Create an array and call it `distances_from_origin` by mapping over the points with the `.map` method and returning the distance between the point and origin, (0,0).
 # Distance from origin can be found with the equation `distance_from_origin = Math.sqrt(x_val ** 2 + y_val ** 2)`
 
+puts distance_from_origin = {
+
+
+}
--- code/fundamentals.rb
+++ code/fundamentals.rb
@@ -8,8 +8,16 @@ puts message
 
 # 1. Create a variable `my_favorite_animal` and assign it the name of your favorite animal
 
+puts my_favorite_animal = "Tiger"
+
 # 2. Print "My favorite animal is <your-favorite-animal>" by printing a string with `my_favorite_animal` interpolated into it.
+
+print "My favorite animal is" + "my_favorite_animal"
 
 # 3. Write a method `make_noise` which prints the string "BANG!"
 
+make_noise = print ="BANG!"
+
 # 4. Write a method `count_down` which takes one argument and prints every number from it to 0. Assume `count_down` will only be called with positive integers
+
+count_down = get print
