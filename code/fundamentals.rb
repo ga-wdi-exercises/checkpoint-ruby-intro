9c265d842d34e070e63f2f24d93ffef164d230e0
Omar Chaudhry <omarchaudhry.1991@gmail.com>
2017-04-10 09:12:23 -0400

    something

--- code/fundamentals.rb
+++ code/fundamentals.rb
@@ -7,9 +7,9 @@ message = "Be sure to run your code regularly to see that it works. Keep trying 
 puts message
 
 # 1. Create a variable `my_favorite_animal` and assign it the name of your favorite animal
-
+my_favorite_animal = "bear"
 # 2. Print "My favorite animal is <your-favorite-animal>" by printing a string with `my_favorite_animal` interpolated into it.
-
+puts my_favorite_animal = "My favorite animal is" my_favorite_animal
 # 3. Write a method `make_noise` which prints the string "BANG!"
 
 # 4. Write a method `count_down` which takes one argument and prints every number from it to 0. Assume `count_down` will only be called with positive integers
